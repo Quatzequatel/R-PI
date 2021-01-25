cae49b1ed24696eb18358cc84d3cadaf226b2630
quatzequatel <quatzequatel@hotmail.com>
2021-01-25 09:46:18 -0800

    addedd files

---
+++ firstapp/program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace firstapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world.");
+            //blah blah
+
+            Console.Readkey();
+        }
+    }
+}
